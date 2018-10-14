aee0f2051c8b57de9ed686eb831ab9beb30f20c8
Erez Giladi <Erez.Giladi@Tradefxl.com>
2018-10-14 17:34:37 +0300

    - initital

---
+++ seven-segment-display/src/App.vue
@@ -0,0 +1,85 @@
+<template>
+  <div id="app">
+    <digit :value="h1" class="dig" />
+    <digit :value="h2" class="dig" />
+    <div class="dots blink_me">
+      <div class="circle"></div>
+      <div class="circle"></div>
+    </div>
+    <digit :value="m1" class="dig" />
+    <digit :value="m2" class="dig" />
+    <div class="dots blink_me">
+      <div class="circle"></div>
+      <div class="circle"></div>
+    </div>
+    <digit :value="s1" class="dig" />
+    <digit :value="s2" class="dig" />
+  </div>
+</template>
+
+<script>
+import Digit from './components/Digit.vue'
+function addZero(i) {
+    if (i < 10) {
+        i = "0" + i;
+    }
+    return i;
+}
+export default {
+  name: 'app',
+  components: {
+    Digit
+  },
+  data: function () {
+    return {
+      h1:0,
+      h2:0,
+      m1:0,
+      m2:0,
+      s1:0,
+      s2:0
+    };
+  },
+  mounted: function () {
+    setInterval(function (){
+      let d = new Date();
+      var h = String(addZero(d.getHours()));
+      this.h1 = h[0];this.h2 = h[1];
+      var m = String(addZero(d.getMinutes()));
+      this.m1 = m[0];this.m2 = m[1];
+      var s = String(addZero(d.getSeconds()));
+      this.s1 = s[0];this.s2 = s[1];
+    }.bind(this), 1000);
+  }
+
+}
+</script>
+
+<style>
+#app {
+  display: flex;
+}
+.dig{
+  margin-right: 50px;
+}
+.dots{
+  padding-top:110px
+}
+.circle {
+  width: 25px;
+  height: 25px;
+  background: #990000;
+  border-radius: 50%;
+  box-shadow: 0px 0px 25px #990000;
+  margin-bottom: 40px;
+}
+.blink_me {
+  animation: blinker 0.5s linear infinite;
+}
+
+@keyframes blinker {
+  50% {
+    opacity: 0.2;
+  }
+}
+</style>
---
+++ seven-segment-display/src/components/Digit.vue
@@ -0,0 +1,99 @@
+<template>
+
+  <div class="digit">
+    <div v-if="sectionA" class="segment A"></div>
+    <div v-if="sectionB" class="segment B"></div>
+    <div v-if="sectionC" class="segment C"></div>
+    <div v-if="sectionD" class="segment D"></div>
+    <div v-if="sectionE" class="segment E"></div>
+    <div v-if="sectionF" class="segment F"></div>
+    <div v-if="sectionG" class="segment G"></div>
+  </div>
+
+</template>
+
+<script>
+const Numbers = {
+  '0': 'ABCDEF',
+  '1': 'BC',
+  '2': 'ABGED',
+  '3': 'ABCDG',
+  '4': 'BCFG',
+  '5': 'ACDFG',
+  '6': 'ACDEFG',
+  '7': 'ABC',
+  '8': 'ABCDEFG',
+  '9': 'ABCFG',
+}
+
+export default {
+  name: 'Digit',
+  props: {
+    value: Number
+  },
+  computed:{
+    sectionA: function (){ return Numbers[this.value].indexOf('A') >= 0; },
+    sectionB: function (){ return Numbers[this.value].indexOf('B') >= 0; },
+    sectionC: function (){ return Numbers[this.value].indexOf('C') >= 0; },
+    sectionD: function (){ return Numbers[this.value].indexOf('D') >= 0; },
+    sectionE: function (){ return Numbers[this.value].indexOf('E') >= 0; },
+    sectionF: function (){ return Numbers[this.value].indexOf('F') >= 0; },
+    sectionG: function (){ return Numbers[this.value].indexOf('G') >= 0; }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .digit{
+    position: relative;
+    height:285px;
+    width: 155px;
+  }
+  .segment {
+    height: 125px;
+    width: 15px;
+    background-color: #990000;
+    border-radius: 9999px;
+    box-shadow: 0px 0px 25px #990000;
+  }
+
+  .A {
+    position: absolute;
+    top: -45px;
+    left: 90px;
+    transform: rotate(90deg);
+  }
+  .B {
+    position: absolute;
+    top: 20px;
+    left: 160px;
+  }
+  .C {
+    position: absolute;
+    top: 150px;
+    left: 160px;
+  }
+  .D {
+    position: absolute;
+    top: 215px;
+    left: 90px;
+    transform: rotate(90deg);
+  }
+  .E {
+    position: absolute;
+    top: 150px;
+    left: 20px;
+  }
+  .F {
+    position: absolute;
+    top: 20px;
+    left: 20px;
+  }
+  .G {
+    position: absolute;
+    top: 85px;
+    left: 90px;
+    transform: rotate(90deg);
+  }
+</style>
